d94d03631faa1bf1e6449bb4ae7855874c36f499
Tobias Landenberger <tobias.landenberger@h-net.com>
2024-01-08 14:38:50 +0100

    feature: effects

--- Angular/src/app/features/courses/courses.component.ts
+++ Angular/src/app/features/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForOf, NgIf } from '@angular/common';
 import { delay, of, tap } from 'rxjs';
@@ -15,6 +15,8 @@ import { delay, of, tap } from 'rxjs';
 export class CoursesComponent implements OnInit {
     courses = signal<Course[]>([]);
     filter = signal<string>('');
+    selectedCourse = signal<Course | null>(null);
+    courseContent = signal<CourseContent | null>(null);
 
     filteredCourses = computed(() =>
         this.courses().filter((course) =>
@@ -22,11 +24,22 @@ export class CoursesComponent implements OnInit {
         )
     );
 
+    constructor() {
+        effect(() => {
+            this.fetchCourseContent(this.selectedCourse()).then(res => this.courseContent.set({
+                content: res || '',
+                status: 'LOADED'
+            }));
+        }, {allowSignalWrites: true});
+    }
+
     ngOnInit() {
         this.fetchCourses()
             .pipe(tap((courses) => this.courses.set(courses)))
             .subscribe();
     }
+
+    onCourseClick = (course: Course) => this.selectedCourse.set(course);
 
     private fetchCourses() {
         return of([
@@ -50,6 +63,24 @@ export class CoursesComponent implements OnInit {
             },
         ] as Course[]).pipe(delay(2000));
     }
+
+    private async fetchCourseContent(course: Course | null) {
+        this.courseContent.update(content => content ? ({...content, status: 'LOADING'}) : null);
+
+        if (course) {
+            return new Promise<string>(resolve => {
+                setTimeout(() => {
+                    const content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
+                        'Sed non risus. Suspendisse lectus tortor, dignissim sit amet, ' +
+                        'adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.'
+
+                    resolve(content);
+                }, 2000);
+            });
+        } else {
+            return null;
+        }
+    }
 }
 
 export type Course = {
@@ -57,4 +88,9 @@ export type Course = {
     title: string;
     description: string;
     category: string;
+};
+
+export type CourseContent = {
+    content: string;
+    status: 'LOADING' | 'LOADED' | 'ERROR';
 };
