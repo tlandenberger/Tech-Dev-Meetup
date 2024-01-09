f35a76d61f1e70e8c0c12ec26147853dd7ddfad4
Tobias Landenberger <tobias.landenberger@h-net.com>
2024-01-09 07:44:15 +0100

    feature: use withEntities in store

--- Angular/src/app/features/courses/CourseStore.ts
+++ Angular/src/app/features/courses/CourseStore.ts
@@ -1,75 +1,113 @@
 import {
-  patchState,
-  signalStore,
-  withComputed,
-  withHooks,
-  withMethods,
-  withState
-} from "@ngrx/signals";
-import { computed, inject } from "@angular/core";
-import { rxMethod } from "@ngrx/signals/rxjs-interop";
-import { Course, CourseContent, CourseService } from "./course.service";
-import { switchMap, tap } from "rxjs";
+    patchState,
+    signalStore,
+    withComputed,
+    withHooks,
+    withMethods,
+    withState,
+} from '@ngrx/signals';
+import { computed, inject } from '@angular/core';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { Course, CourseContent, CourseService } from './course.service';
+import { switchMap, tap } from 'rxjs';
+import {
+    addEntity,
+    removeEntity,
+    setEntities,
+    updateEntity,
+    withEntities,
+} from '@ngrx/signals/entities';
 
 export const CourseStore = signalStore(
-  { providedIn: "root" },
-  withState({
-    status: "INIT" as "INIT" | "LOADING" | "LOADED" | "ERROR",
-    courses: [] as Course[],
-    filter: "" as string,
-    selectedCourseId: null as number | null,
-    courseContent: null as CourseContent | null
-  }),
-  withComputed(({ courses, filter }) => ({
-    filteredCourses: computed(() =>
-      courses().filter((course) =>
-        filter() === "" ? course : course.category === filter()
-      )
-    )
-  })),
-  withMethods((state) => {
-    const courseService = inject(CourseService);
+    { providedIn: 'root' },
+    withState({
+        status: 'INIT' as 'INIT' | 'LOADING' | 'LOADED' | 'ERROR',
+        filter: '' as string,
+        selectedCourseId: null as number | null,
+        courseContent: null as CourseContent | null,
+    }),
+    withEntities<Course>(),
+    withComputed(({ filter, entities }) => ({
+        filteredCourses: computed(() =>
+            entities().filter((course) =>
+                filter() === '' ? course : course.category === filter()
+            )
+        ),
+    })),
+    withMethods((state) => {
+        const courseService = inject(CourseService);
 
-    return {
-      setFilter: (filter: string) => patchState(state, { filter }),
+        return {
+            setFilter: (filter: string) => patchState(state, { filter }),
 
-      selectCourse: (id: number | null) => {
-        if (state.selectedCourseId() === id) return;
-        patchState(state, { selectedCourseId: id });
-      },
+            selectCourse: (id: number | null) => {
+                if (state.selectedCourseId() === id) return;
+                patchState(state, { selectedCourseId: id });
+            },
 
-      load: rxMethod<void>(() => {
-        return courseService.fetch().pipe(
-          tap((res) =>
-            patchState(state, {
-              courses: res,
-              status: "LOADED"
-            })
-          )
-        );
-      }),
+            addCourse: () => {
+                const newCourse = {
+                    id: Math.floor(Math.random() * 1000),
+                    title: 'New Course',
+                    description: 'New Course Description',
+                    category: 'BEGINNER',
+                } as Course;
+                patchState(state, addEntity(newCourse));
+            },
 
-      loadContent: rxMethod<number | null>((courseId$) =>
-        courseId$.pipe(
-          tap(() =>
-            patchState(state, {
-              courseContent: { content: "", status: "LOADING" }
-            })
-          ),
-          switchMap((courseId) => courseService.fetchContent(courseId)),
-          tap((res) =>
-            patchState(state, {
-              courseContent: res
-            })
-          )
-        )
-      )
-    };
-  }),
-  withHooks({
-    onInit({ load, loadContent, selectedCourseId }) {
-      load();
-      loadContent(selectedCourseId);
-    }
-  })
+            updateCourse: (course: Course) => {
+                const newTitle = course.title + ' Updated';
+                patchState(
+                    state,
+                    {},
+                    updateEntity({
+                        id: course.id,
+                        changes: { title: newTitle },
+                    })
+                );
+            },
+
+            removeCourse: (course: Course) => {
+                patchState(state, {}, removeEntity(course.id));
+            },
+
+            load: rxMethod<void>(() => {
+                return courseService
+                    .fetch()
+                    .pipe(
+                        tap((res) =>
+                            patchState(
+                                state,
+                                { status: 'LOADED' },
+                                setEntities(res)
+                            )
+                        )
+                    );
+            }),
+
+            loadContent: rxMethod<number | null>((courseId$) =>
+                courseId$.pipe(
+                    tap(() =>
+                        patchState(state, {
+                            courseContent: { content: '', status: 'LOADING' },
+                        })
+                    ),
+                    switchMap((courseId) =>
+                        courseService.fetchContent(courseId)
+                    ),
+                    tap((res) =>
+                        patchState(state, {
+                            courseContent: res,
+                        })
+                    )
+                )
+            ),
+        };
+    }),
+    withHooks({
+        onInit({ load, loadContent, selectedCourseId }) {
+            load();
+            loadContent(selectedCourseId);
+        },
+    })
 );
--- Angular/src/app/features/courses/courses.component.ts
+++ Angular/src/app/features/courses/courses.component.ts
@@ -1,22 +1,18 @@
 import { Component, inject, Signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {CourseStore} from "./CourseStore";
-import {Course} from "./course.service";
-import {CourseStore} from "./CourseStore";
-import {Course} from "./course.service";
+import { CourseStore } from './CourseStore';
+import { Course } from './course.service';
 
 @Component({
     selector: 'app-courses',
     standalone: true,
-    imports: [
-        FormsModule,
-    ],
+    imports: [FormsModule],
     templateUrl: './courses.component.html',
     styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
     private store = inject(CourseStore);
-    courses = this.store.courses;
+    courses = this.store.entities;
     filteredCourses: Signal<Course[]> = this.store.filteredCourses;
     filter = this.store.filter;
     courseContent = this.store.courseContent;
@@ -24,4 +20,10 @@ export class CoursesComponent {
     onCourseClick = (course: Course) => this.store.selectCourse(course.id);
 
     onFilterChange = (filter: string) => this.store.setFilter(filter);
+
+    onAddCourseClick = () => this.store.addCourse();
+
+    onUpdateCourseClick = (course: Course) => this.store.updateCourse(course);
+
+    onDeleteCourseClick = (course: Course) => this.store.removeCourse(course);
 }
