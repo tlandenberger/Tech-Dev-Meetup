bb63e43fe561e61a763f683c93da45b7a7dad7c3
Tobias Landenberger <tobias.landenberger@h-net.com>
2024-01-16 12:49:29 +0100

    feature: use Observable response

--- Angular/src/app/features/courses/courses.component.spec.ts
+++ Angular/src/app/features/courses/courses.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CoursesComponent } from './courses.component';
 
 describe('CoursesComponent', () => {
-  let component: CoursesComponent;
-  let fixture: ComponentFixture<CoursesComponent>;
+    let component: CoursesComponent;
+    let fixture: ComponentFixture<CoursesComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CoursesComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(CoursesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CoursesComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(CoursesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
